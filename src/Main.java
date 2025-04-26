4ca989dbfa51725286f982d2ec91426c2a6c9c6a
Sirmrmagoo <Sirmrmagoo@gmail.com>
2025-04-25 18:26:29 -0700

    HUGE CODE CLEAN and fix for GUI elements not showing till hover

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Game game = new Game();
+    public static void main(String[] args) throws IOException, FontFormatException {
+
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(Main.class.getResource("/res/7.png")));
+        JFrame frame = new JFrame("Slot Machine");
+        Panel panel = new Panel();
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 
--- src/Panel.java
+++ src/Panel.java
@@ -8,92 +8,76 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 
-public class Game extends JFrame implements ChangeListener, ActionListener, Runnable {
+public class Panel extends JPanel implements ActionListener, ChangeListener {
 
-    boolean pulled = false;
-
-    int slot1;
-    int slot2;
-    int slot3;
+    int slot1 = 0;
+    int slot2 = 0;
+    int slot3 = 0;
     int[] slots = new int[3];
 
-    int money = 500;
+    final int PANEL_WIDTH = 500;
+    final int PANEL_HEIGHT = 500;
 
-    Panel panel;
-    JSlider bet;
+
+    int money = 750;
+
+
     JLabel betNum;
-    JButton button;
+    JSlider bet;
+    Image slotIdle;
+    JButton pullButton;
+    Timer timer;
 
-    Thread gameThread;
+    Panel() {
+        timer = new Timer(500,null);
 
-    Game() {
 
-        panel = new Panel();
-        panel.setPreferredSize(new Dimension(500,500));
-
-        this.setTitle("Slot Machine");
-
-        this.setSize(500,500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
+        this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        this.setBackground(new Color(240,235,210));
         this.setLayout(null);
-        ImageIcon image = new ImageIcon(Objects.requireNonNull(Main.class.getResource("/res/7.png")));
-        this.setIconImage(image.getImage());
+
+        slotIdle = new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slot.png"))).getImage();
 
 
-        bet = new JSlider(0,money,0);
+        pullButton = new JButton("p");
+        pullButton.setLayout(null);
+        pullButton.addActionListener(this);
+        pullButton.setBounds(60,60,120,40);
+        this.add(pullButton);
+
+
+        bet = new JSlider(0, money,0);
         bet.addChangeListener(this);
-        bet.setBounds(190,425,100,15);
+        bet.setLayout(null);
+        bet.setBounds(10,10,100,15);
+        this.add(bet);
 
         betNum = new JLabel();
         betNum.setText("Bet: " + bet.getValue());
-        betNum.setBounds(170,400,50,40);
-
-        button  = new JButton();
-        button.addActionListener(this);
-        button.setLayout(null);
-        button.setBounds(10,10,240,135);
-
-
-        this.add(panel);
-        this.add(button);
-        this.add(bet);
+        betNum.setLayout(null);
+        betNum.setBounds(10,10,50,40);
         this.add(betNum);
+   }
 
-
-        this.setVisible(true);
-    }
-
-    public void startGameThread() {
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
-
-    @Override
-    public void stateChanged(ChangeEvent changeEvent) {
-        betNum.setText("Bet: " + bet.getValue());
-    }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
-        Random rand = new Random();
-
-        if (actionEvent.getSource().equals(button)) {
-
-            slot1 = rand.nextInt(5)+1;
-            slot2 = rand.nextInt(5)+1;
-            slot3 = rand.nextInt(5)+1;
-            slots[0] = slot1;
-            slots[1] = slot2;
-            slots[2] = slot3;
-            Arrays.sort(slots);
-
-            System.out.println(slot1+""+slot2+""+slot3);
-
-
+        if (actionEvent.getSource().equals(pullButton)) {
             if (money > 0) {
+                Random rand = new Random();
+                slot1 = rand.nextInt(5) + 1;
+                slot2 = rand.nextInt(5) + 1;
+                slot3 = rand.nextInt(5) + 1;
+
+                slots[0] = slot1;
+                slots[1] = slot2;
+                slots[2] = slot3;
+
+                Arrays.sort(slots);
+
+                System.out.println(slot1 + "" + slot2 + "" + slot3);
+
                 if (slots[0] == 5 && slots[0] == slots[1] && slots[0] == slots[2]) {
                     System.out.println("777 Bet 50x");
                     money += bet.getValue() * 50;
@@ -118,17 +102,25 @@ public class Game extends JFrame implements ChangeListener, ActionListener, Runn
                     money -= bet.getValue();
                     bet.setMaximum(money);
                 }
-            }else {
+            } else {
                 System.out.println("You Lost All Your Money, Go Cry");
             }
-
         }
     }
 
     @Override
-    public void run() {
-        while (gameThread != null) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); // paints background and child components properly
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(slotIdle, 50, 50, null); // draws under the button
+    }
 
-        }
+    @Override
+    public void stateChanged (ChangeEvent changeEvent){
+
+        betNum.setText("Bet: " + bet.getValue());
+    }
+
+    public void startGameThread () {
     }
 }
