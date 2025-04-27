73ab2a0d18a377aed42c35e354972863476ae26e
Sirmrmagoo <Sirmrmagoo@gmail.com>
2025-04-26 22:29:02 -0700

    Started on showing what you got on the slots instead of in the  console

--- src/Panel.java
+++ src/Panel.java
@@ -4,7 +4,6 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Objects;
@@ -12,8 +11,8 @@ import java.util.Random;
 
 public class Panel extends JPanel implements ActionListener, ChangeListener {
 
-    int slot1 = 0;
-    int slot2 = 0;
+    int slot1 = 1;
+    int slot2 = 2;
     int slot3 = 0;
     int[] slots = new int[3];
 
@@ -28,21 +27,12 @@ public class Panel extends JPanel implements ActionListener, ChangeListener {
 
     JLabel betNum;
     JSlider bet;
-    Image slot;
+    Image slotMachine;
+    Image slot1icon;
+    Image slot2icon;
+    Image slot3icon;
     JButton pullButton;
     Timer timer;
-
-
-    public void animationFrames() {
-        slot = new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slotMachine/slot.png"))).getImage();
-
-        frames = new Image[12];
-        for (int i = 0; i < 12; i++) {
-            frames[i] = new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slotMachine/slotFrame" + i + ".png"))).getImage();
-
-        }
-        timer = new Timer(100, this);
-    }
 
 
     Panel() {
@@ -98,64 +88,85 @@ public class Panel extends JPanel implements ActionListener, ChangeListener {
 
                 timer = new Timer(100, e -> {
 
-                    if (currentFrame < frames.length - 1 ) {
+                    if (currentFrame < frames.length - 1) {
                         currentFrame++;
                         repaint();
-                    }else {
+                    } else {
                         timer.stop();
                         animationActive = false;
                         currentFrame = 0;
                         repaint();
+                        showSlots();
                     }
-
                 });
                 timer.start();
-
-                Random rand = new Random();
-
-                slot1 = rand.nextInt(5) + 1;
-                slot2 = rand.nextInt(5) + 1;
-                slot3 = rand.nextInt(5) + 1;
-
-                slots[0] = slot1;
-                slots[1] = slot2;
-                slots[2] = slot3;
-
-                Arrays.sort(slots);
-
-                System.out.println(slot1 + "" + slot2 + "" + slot3);
-
-                if (slots[0] == 5 && slots[0] == slots[1] && slots[0] == slots[2]) {
-                    System.out.println("777 Bet 50x");
-                    money += bet.getValue() * 50;
-                    bet.setMaximum(money);
-                } else if (slots[0] == 4 && slots[0] == slots[1] && slots[0] == slots[2]) {
-                    System.out.println("All 3 Bells Bet 25x");
-                    money += bet.getValue() * 25;
-                    bet.setMaximum(money);
-                } else if ((slots[0] == 1 && slots[0] == slots[1] && slots[1] == slots[2]) ||
-                        (slots[0] == 2 && slots[0] == slots[1] && slots[1] == slots[2]) ||
-                        (slots[0] == 3 && slots[0] == slots[1] && slots[1] == slots[2])) {
-                    System.out.println("All Same Fruit Bet 10x");
-                    money += bet.getValue() * 10;
-                    bet.setMaximum(money);
-                } else if (slots[0] == 1 && slots[1] == 2 && slots[2] == 3) {
-                    System.out.println("All Fruit Bet 2x");
-                    money += bet.getValue() * 2;
-                    System.out.println(money);
-                    bet.setMaximum(money);
-                } else {
-                    System.out.println("You Lost");
-                    money -= bet.getValue();
-                    bet.setMaximum(money);
-                }
-            } else {
+            }
+            else{
                 System.out.println("You Lost All Your Money, Go Cry");
             }
         }
     }
 
 
+    public void showSlots() {
+
+        Random rand = new Random();
+
+        slot1 = rand.nextInt(5) + 1;
+        slot2 = rand.nextInt(5) + 1;
+        slot3 = rand.nextInt(5) + 1;
+
+        slots[0] = slot1;
+        slots[1] = slot2;
+        slots[2] = slot3;
+
+        Arrays.sort(slots);
+
+        System.out.println(slot1 + "" + slot2 + "" + slot3);
+
+        animationFrames();
+
+        if (slots[0] == 5 && slots[0] == slots[1] && slots[0] == slots[2]) {
+            System.out.println("777 Bet 50x");
+            money += bet.getValue() * 50;
+            bet.setMaximum(money);
+        } else if (slots[0] == 4 && slots[0] == slots[1] && slots[0] == slots[2]) {
+            System.out.println("All 3 Bells Bet 25x");
+            money += bet.getValue() * 25;
+            bet.setMaximum(money);
+        } else if ((slots[0] == 1 && slots[0] == slots[1] && slots[1] == slots[2]) ||
+                (slots[0] == 2 && slots[0] == slots[1] && slots[1] == slots[2]) ||
+                (slots[0] == 3 && slots[0] == slots[1] && slots[1] == slots[2])) {
+            System.out.println("All Same Fruit Bet 10x");
+            money += bet.getValue() * 10;
+            bet.setMaximum(money);
+        } else if (slots[0] == 1 && slots[1] == 2 && slots[2] == 3) {
+            System.out.println("All Fruit Bet 2x");
+            money += bet.getValue() * 2;
+            System.out.println(money);
+            bet.setMaximum(money);
+        } else {
+            System.out.println("You Lost");
+            money -= bet.getValue();
+            bet.setMaximum(money);
+        }
+    }
+
+
+
+    public void animationFrames() {
+
+        //SLOT1ICON GET THE RES PATH WITH THE SLOT1 INT
+        slotMachine = new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slotMachine/slot.png"))).getImage();
+        slot1icon= new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slots/"+slot1+".png"))).getImage();
+        slot2icon= new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slots/"+slot2+".png"))).getImage();
+
+        frames = new Image[12];
+        for (int i = 0; i < 12; i++) {
+            frames[i] = new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/slotMachine/slotFrame" + i + ".png"))).getImage();
+        }
+        timer = new Timer(100, this);
+    }
 
 
     @Override
@@ -164,8 +175,12 @@ public class Panel extends JPanel implements ActionListener, ChangeListener {
         Graphics2D g2 = (Graphics2D) g;
         if(animationActive) {
             g2.drawImage(frames[currentFrame],50,50,null);
+
         }else {
-            g2.drawImage(slot, 50, 50, null);
+            g2.drawImage(slotMachine, 50, 50, null);
+            g2.drawImage(slot1icon,100,50,null);
+            g2.drawImage(slot2icon,150,50,null);
+
         }
     }
 
